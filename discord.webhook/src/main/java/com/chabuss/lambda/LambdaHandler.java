cd87f9036cd479d7488ffc3c6effa3bf3f285ef1
Charlinho <charles.buss@beetools.com.br>
2019-05-19 22:32:16 -0300

    - First use of DiscordHookMessage and DiscordEmbed with CodeCommit triggers.

--- discord.webhook/src/main/java/com/chabuss/lambda/LambdaHandler.java
+++ discord.webhook/src/main/java/com/chabuss/lambda/LambdaHandler.java
@@ -4,47 +4,50 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.CodeCommitEvent;
 import com.amazonaws.services.lambda.runtime.events.CodeCommitEvent.Record;
+import com.amazonaws.services.lambda.runtime.events.CodeCommitEvent.Reference;
+import com.chabuss.discord.DiscordEmbed;
+import com.chabuss.discord.DiscordHookMessage;
+import com.chabuss.http.HttpRequest;
 
 import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 
 public class LambdaHandler implements RequestHandler<CodeCommitEvent, Void> {
 
 	@Override public Void handleRequest(CodeCommitEvent input, Context context) {
 		
-		System.out.println(Json.encodePrettily(input));
+		
+		Record record = input.getRecords().get(0);
+		
+		DiscordHookMessage message = new DiscordHookMessage();
+		
+		message.setUserName("AWS Lambda");
+		
+		DiscordEmbed embed = new DiscordEmbed()
+				.addField("Region", record.getAwsRegion(), true)
+				.addField("Event name", record.getEventName(), true)
+				.addField("User", record.getUserIdentityArn().substring(record.getUserIdentityArn().indexOf("/")), true)
+				.setFooter("AWS Code Commit", null);
+		
+		message.addEmbed(embed);
+		
+		for(Reference reference : record.getCodeCommit().getReferences())
+			message.addEmbed(new DiscordEmbed()
+					.addField("Commit", reference.getCommit(), true));
+		
+		System.out.println(Json.encode(input));
+		System.out.println("WebHook "+record.getCustomData());
+		
+		try {
+			HttpRequest request = HttpRequest.post(record.getCustomData(), message.toJsonObject().encode());
+			request.execute();
+			System.out.println("Response status code: "+request.getResponseStatusCode());
+			System.out.println("Response status code: "+request.getResponseBody());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
 
-	
-	public JsonObject getDiscordMessageGromRecord(Record record) {
-		
-		String username = "";
-		
-		JsonArray embeds = new JsonArray();
-		JsonObject message = new JsonObject()
-				.put("username", "BOTinho")
-				.put("avatar_url", "https://s3.us-east-2.amazonaws.com/public.beetools/images/beetools_icon_orange.png")
-				.put("embeds", new JsonArray().add(
-							new JsonObject()
-								.put("author", new JsonObject()
-										.put("name", username))
-								.put("title", record.getEventName())
-								.put("color", 7645483)
-								.put("fields", "")
-							));	
-		
-		
-		return message;
-	}
-	
-	private JsonObject addField(String name, String value, boolean inline) {
-		return new JsonObject()
-				.put("name", name)
-				.put("value", value)
-				.put("inline", inline);
-	}
-	
 }
